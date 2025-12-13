abab948b58829a54faef2ad7a125ab849a25a0ec
Alper KIZIL <alperkizil92@gmail.com>
2025-12-13 15:04:45 +0300

    Revert "Claude/maven local jar config 01 uv pt rm41 zt qh ag utkd swv6"

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Run the application - no Maven required
-
-set LIB_DIR=lib
-set OUT_DIR=target\classes
-set MAIN_CLASS=com.cloudsimulator.gui.Launcher
-
-java --module-path %LIB_DIR% --add-modules javafx.controls,javafx.fxml -cp "%OUT_DIR%" %MAIN_CLASS%
